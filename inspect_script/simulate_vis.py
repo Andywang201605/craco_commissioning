6776c8929d71fa0731eff6605743a50b456d21b9
Andywang201605 <ztwang201605@gmail.com>
2023-03-20 23:57:28 +1100

    output noise corr as well

--- inspect_script/simulate_vis.py
+++ inspect_script/simulate_vis.py
@@ -25,37 +25,42 @@ def sim_ant_data(nant, nt, srcflux=0.01, ave=True):
     
     logger.info("performing correlation...")
     c_ave = np.array([(a[i] * a[j].conj()).mean() for i in range(nant) for j in range(i, nant)])
+    n_ave = np.array([(n[i] * n[j].conj()).mean() for i in range(nant) for j in range(i, nant)])
         
-    return c_ave.reshape(-1, 1)
+    return c_ave.reshape(-1, 1), n_ave.reshape(-1, 1)
 
 def sim_series_data(nant=36, nt=20, nave=1000000, srcflux=0.01):
     logger.info(f"creating simulated sigal from {nant} antennas, with {nave/1e6}M samples averaging, and {nt} integrations...")
     logger.info(f"source signal flux is {srcflux} unit...")
     
-    c_data = []
+    c_data = []; n_data = []
     for i in range(nt):
         if i % 5 == 0:
             logger.info("*" * 80); logger.info(f"{i} integration..."); logger.info("*" * 80)
+        c_, n_ = sim_ant_data(nant, nave, srcflux)
         
-        c_data.append(sim_ant_data(nant, nave, srcflux))
+        c_data.append(c_); n_data.append(n_)
 
-    return c_data
+    return c_data, n_data
 
-def dump_data(c_data, fname):
+def dump_data(c_data, n_data, fname):
     ### combine data together...
     c_ = np.concatenate(c_data, axis = 1)
+    n_ = np.concatenate(n_data, axis = 1)
     logger.info(f"saving simulated data to {fname}... final data shape is {c_.shape}")
     np.save(fname, c_)
+    logger.info(f"saving corresponding noise data...")
+    np.save(fname.replace(".npy", "_n.npy"), n_)
     
     
 def main(folder="./sim", nant=36, nt=100, nave=1000000, srcflux=0.01, prefix="sim"):
     if srcflux < 0.01: return None
     if not os.path.exists(folder): os.makedirs(folder)
         
-    c_data =sim_series_data(nant, nt, nave, srcflux)
+    c_data, n_data =sim_series_data(nant, nt, nave, srcflux)
     fname = f"{prefix}_nant{nant}_nt{nt}_nave{nave/1e6:.0f}M_srcflux{srcflux:.2f}.npy"
     
-    dump_data(c_data, f"{folder}/{fname}")
+    dump_data(c_data, n_data, f"{folder}/{fname}")
     
     
     
